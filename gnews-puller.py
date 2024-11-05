af26e5b2a0ee05a261b2c887c00aea2733c380b7
Tony <konigderspiele@live.com>
2024-11-05 20:45:34 +0000

    Ask claude to output a json and omit unconfirmed events

--- gnews-puller.py
+++ gnews-puller.py
@@ -11,9 +11,11 @@ from bs4 import BeautifulSoup
 
 GNEWS_API_KEY = os.environ.get("GNEWS_API_KEY")
 CLAUDE_API_KEY = os.environ.get("CLAUDE_API_KEY")
-CLAUDE_SONNET_MODEL = "claude-3-5-haiku-latest"
+CLAUDE_MODEL = "claude-3-5-haiku-latest"
+'''
 LLM_PROMPT_INTRO = (
-	"Analyze the following articles about election news and output a list of the following items. If the result cannot be confirmed for a specific item, write IDK instead of the options given. Separate each item. Do not assume or make projections for results. The articles are as follows: "
+	"Analyze the following articles within the <text> tag about election news and output a numbered list of the items in the <format> tag. If the result cannot be confirmed for a specific item, write IDK instead of the options given. Only consider an election a victory if it's explicitly mentioned and do not make projections or base the answers on expectations. Only write one token for each item, e.g., 1. IDK 2. DSEN"
+	"<format>"
 	"1. If Kamala wins the presidency, write DPRES here, and if Trump wins, write RPRES here "
 	"2. If Democrats win the majority of the seats in the Senate, write DSEN here, and if the Republicans win the majority, write RSEN here "
 	"3. If Democrats win the majority of the seats in the House of Representatives, write DHOUSE here, and if the Republicans win the majority, write RHOUSE here "
@@ -76,13 +78,80 @@ LLM_PROMPT_INTRO = (
 	"60. If Slotkin wins the Senate election in Michigan, write 1MI here, and if Rogers wins, write 2MI here "
 	"61. If Gallego wins the Senate election in Arizona, write 1AZ here, and if Lake wins, write 2AZ here "
 	"62. If Casey Jr. wins the Senate election in Pennsylvania, write 1PA here, and if McCormick wins, write 2PA here "
+	"</format>"
+)
+'''
+LLM_PROMPT_INTRO = (
+	"Analyze the following articles within the <text> tag about election news and output a json with one key for each item in the list. The key should be the number of the item in the list. The value should be made according to the instructions for each item. If the result cannot be confirmed for a specific item, ignore that key and omit it from the json output. Only consider an election a victory if it's EXPLICITLY confirmed. Do NOT make projections or base the answers on expectations. For example, the first two items could be {'1': 'DPRES', '2': 'DSEN'}\n"
+	"1: (DPRES if Kamala wins the presidency/RPRES if Trump wins the presidency)\n"
+	"2: (DSEN if Democrats win the majority of the seats in the Senate/RSEN if Republicans win the majority of the seats)\n"
+	"3: (DHOUSE if Democrats win the majority of the seats in the House of Representatives/RHOUSE if Republicans win the majority)\n"
+	"4: (DAL if Kamala wins the Alabama election/RAL if Trump wins)\n"
+	"5: (DAK if Kamala wins the Alaska election/RAK if Trump wins)\n"
+	"6: (DAZ if Kamala wins the Arizona election/RAZ if Trump wins)\n"
+	"7: (DAR if Kamala wins the Arkansas election/RAR if Trump wins)\n"
+	"8: (DCA if Kamala wins the California election/RCA if Trump wins)\n"
+	"9: (DCO if Kamala wins the Colorado election/RCO if Trump wins)\n"
+	"10: (DCT if Kamala wins the Connecticut election/RCT if Trump wins)\n"
+	"11: (DDE if Kamala wins the Delaware election/RDE if Trump wins)\n"
+	"12: (DFL if Kamala wins the Florida election/RFL if Trump wins)\n"
+	"13: (DGA if Kamala wins the Georgia election/RGA if Trump wins)\n"
+	"14: (DHI if Kamala wins the Hawaii election/RHI if Trump wins)\n"
+	"15: (DID if Kamala wins the Idaho election/RID if Trump wins)\n"
+	"16: (DIL if Kamala wins the Illinois election/RIL if Trump wins)\n"
+	"17: (DIN if Kamala wins the Indiana election/RIN if Trump wins)\n"
+	"18: (DIA if Kamala wins the Iowa election/RIA if Trump wins)\n"
+	"19: (DKS if Kamala wins the Kansas election/RKS if Trump wins)\n"
+	"20: (DKY if Kamala wins the Kentucky election/RKY if Trump wins)\n"
+	"21: (DLA if Kamala wins the Louisiana election/RLA if Trump wins)\n"
+	"22: (DME if Kamala wins the Maine election/RME if Trump wins)\n"
+	"23: (DMD if Kamala wins the Maryland election/RMD if Trump wins)\n"
+	"24: (DMA if Kamala wins the Massachusetts election/RMA if Trump wins)\n"
+	"25: (DMI if Kamala wins the Michigan election/RMI if Trump wins)\n"
+	"26: (DMN if Kamala wins the Minnesota election/RMN if Trump wins)\n"
+	"27: (DMS if Kamala wins the Mississippi election/RMS if Trump wins)\n"
+	"28: (DMO if Kamala wins the Missouri election/RMO if Trump wins)\n"
+	"29: (DMT if Kamala wins the Montana election/RMT if Trump wins)\n"
+	"30: (DNE if Kamala wins the Nebraska election/RNE if Trump wins)\n"
+	"31: (DNV if Kamala wins the Nevada election/RNV if Trump wins)\n"
+	"32: (DNH if Kamala wins the New Hampshire election/RNH if Trump wins)\n"
+	"33: (DNJ if Kamala wins the New Jersey election/RNJ if Trump wins)\n"
+	"34: (DNM if Kamala wins the New Mexico election/RNM if Trump wins)\n"
+	"35: (DNY if Kamala wins the New York election/RNY if Trump wins)\n"
+	"36: (DNC if Kamala wins the North Carolina election/RNC if Trump wins)\n"
+	"37: (DND if Kamala wins the North Dakota election/RND if Trump wins)\n"
+	"38: (DOH if Kamala wins the Ohio election/ROH if Trump wins)\n"
+	"39: (DOK if Kamala wins the Oklahoma election/ROK if Trump wins)\n"
+	"40: (DOR if Kamala wins the Oregon election/ROR if Trump wins)\n"
+	"41: (DPA if Kamala wins the Pennsylvania election/RPA if Trump wins)\n"
+	"42: (DRI if Kamala wins the Rhode Island election/RRI if Trump wins)\n"
+	"43: (DSC if Kamala wins the South Carolina election/RSC if Trump wins)\n"
+	"44: (DSD if Kamala wins the South Dakota election/RSD if Trump wins)\n"
+	"45: (DTN if Kamala wins the Tennessee election/RTN if Trump wins)\n"
+	"46: (DTX if Kamala wins the Texas election/RTX if Trump wins)\n"
+	"47: (DUT if Kamala wins the Utah election/RUT if Trump wins)\n"
+	"48: (DVT if Kamala wins the Vermont election/RVT if Trump wins)\n"
+	"49: (DVA if Kamala wins the Virginia election/RVA if Trump wins)\n"
+	"50: (DWA if Kamala wins the Washington election/RWA if Trump wins)\n"
+	"51: (DWV if Kamala wins the West Virginia election/RWV if Trump wins)\n"
+	"52: (DWI if Kamala wins the Wisconsin election/RWI if Trump wins)\n"
+	"53: (DWY if Kamala wins the Wyoming election/RWY if Trump wins)\n"
+	"54: (1W1 if Baldwin wins the Senate election in Wisconsin/2WI if Hovde wins)\n"
+	"55: (1TX if Cruz wins the Senate election in Texas/2TX if Allred wins)\n"
+	"56: (1OH if Moreno wins the Senate election in Ohio/2OH if Brown wins)\n"
+	"57: (1NE if Fischer wins the Senate election in Nebraska/2NE if Osborn wins)\n"
+	"58: (1MT if Sheehy wins the Senate election in Montana/2MT if Tester wins)\n"
+	"59: (1NV if Rosen wins the Senate election in Nevada/2NV if Brown wins)\n"
+	"60: (1MI if Slotkin wins the Senate election in Michigan/2MI if Rogers wins)\n"
+	"61: (1AZ if Gallego wins the Senate election in Arizona/2AZ if Lake wins)\n"
+	"62: (1PA if Casey Jr. wins the Senate election in Pennsylvania/2PA if McCormick wins)\n"
 )
 
 GNEWS_URL = "https://gnews.io/api/v4/top-headlines"
 PARAMS = {
 	"apikey": GNEWS_API_KEY,
 	"category": "nation",
-	"max": 40,
+	"max": 3,
 	"expand": "content",
 	"country": "us",
 	"nullable": "image,description",
@@ -102,6 +171,7 @@ def sendLLMResults(sock, data):
 
 	conn = None
 	if isSocketActive(sock):
+		print("active")
 		try:
 			conn, _ = sock.accept()
 			conns.append(conn)
@@ -110,6 +180,8 @@ def sendLLMResults(sock, data):
 				conns.remove(conn)
 			traceback.print_exc()
 			printError(e)
+	else:
+		print("inactive")
 
 	for conn in conns:
 		try:
@@ -130,11 +202,17 @@ def runNewsPuller():
 	mostRecentArticles = []
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
-	sock.bind(('localhost', 23456))
-	sock.listen(2)
+	# sock.bind(('localhost', 23456))
+	sock.bind(('0.0.0.0', 23456))
+	sock.listen(1)
+
+	elapsedTime = 1
 
 	while True:
 		try:
+			start = time.time()
+			if elapsedTime < 1:
+				time.sleep(1 - elapsedTime)
 			response = requests.get(GNEWS_URL, params=PARAMS)
 			response.raise_for_status()
 			totalResponse = []
@@ -151,14 +229,16 @@ def runNewsPuller():
 					# totalResponse.append(article['title'])
 			if totalResponse:
 				message = claudeClient.messages.create(
-					model=CLAUDE_SONNET_MODEL,
+					model=CLAUDE_MODEL,
 					max_tokens=2048,
 					messages=[
-						{"role": "user", "content": LLM_PROMPT_INTRO + " ".join(totalResponse)}
+						{"role": "user", "content": LLM_PROMPT_INTRO + "<text>" + " ".join(totalResponse) + "</text>"}
 					]
 				)
-				# sendLLMResults(message.content
+				time.sleep(5)
+				sendLLMResults(sock, message.content[0].text)
 				print(message.content[0].text)
+			elapsedTime = time.time() - start
 		except Exception as e:
 			traceback.print_exc()
 			printError(e)
